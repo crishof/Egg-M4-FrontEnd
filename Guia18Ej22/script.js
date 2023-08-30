e8422816aba7879991f7ab1c6fd477de414c14d3
Cristian.hof <cristianhof@gmail.com>
2023-08-30 16:41:23 +0200

    ej09

--- Guia18Ej22/script.js
+++ Guia18Ej22/script.js
@@ -5,6 +5,6 @@ function mostrarConEspacios(frase) {
     return letrasConEspacios;
 }
 
-var fraseInput = "Ingrese una frase:";
+var fraseInput = prompt("Ingrese una frase:");
 var resultado = mostrarConEspacios(fraseInput);
 console.log("Frase con espacios entre letras:", resultado);
