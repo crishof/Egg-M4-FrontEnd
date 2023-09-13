1a7dfe1699bea36200b241a93e6c038e1300c291
Cristian.hof <cristianhof@gmail.com>
2023-09-13 13:03:26 +0200

    add html

--- Guia18Ej25/script.js
+++ Guia18Ej25/script.js
@@ -1,15 +1,36 @@
 
+console.log("Hola!");
 
 function getFormValores(){
-    let nombre = document.getElementsByName('nombre')[0].value;
-    let apellido = document.getElementsByName('apellido');
+    let formulario = document.getElementById('form1');
+    
+    let nombre = formulario.elements['nombre'].value;
 
-    console.log(nombre);
+    let apellido = formulario.elements["apellido"].value;
+    
+    console.log(nombre,apellido);
 
-    document.getElementById('resultado').innerHTML = "El nombre ingresado es: " + nombre + " " + apellido[0].value
-}
+        document.getElementById('resultado').innerHTML = "El nombre ingresado es: " + nombre + " " + apellido
+
+    // return false;
+            
+    }
+
+// function getFormValores(){
+//     let nombre = document.getElementsByName('nombre')[0].value;
+//     let apellido = document.getElementsByName('apellido');
+
+//     console.log(nombre);
+
+//     document.getElementById('resultado').innerHTML = "El nombre ingresado es: " + nombre + " " + apellido[0].value
+//     return false;
+// }
 
 document.getElementById('form1').addEventListener('submit',function(event){
     event.preventDefault();
-    getFormValores();
+    // getFormValores();
 });
+
+// let forma= document.getElementById('form1');
+// let nombre = forma.elements["nombre"].value;
+// console.log(nombre);
